989531d6e5a364597f0327759122b4afdba33765
estherwanjiku007 <152400135+estherwanjiku007@users.noreply.github.com>
2023-12-09 19:13:51 +0300

    Add files via upload

---
+++ project.js
@@ -0,0 +1,12 @@
+document.querySelector("mybtn1").addEventListener("click",function()){
+    alert("Football,Basetball,Volleyball");
+}
+document.querySelector("mybtn2").addEventListener("click",function()){
+    alert("Dancing,Singing,Hiking");
+}    
+document.querySelector("mybtn3").addEventListener("click",function()){
+    alert("pilau,Spaghetti,Fried chicken");
+}
+document.querySelector("mybtn4").addEventListener("click",function()){ 
+    alert("Umoja Primary School,Kiburia Girls Secondry school");
+}
